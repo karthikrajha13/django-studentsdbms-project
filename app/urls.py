28801e55444b80378dac19a05a20a11420dd7f3e
karthikrajha13 <karthikrajha849@gmail.com>
2026-07-16 18:09:58 +0530

    Navbar Modified

--- app/urls.py
+++ app/urls.py
@@ -3,16 +3,23 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/', views.about),
-    path('contact/', views.contact),
-    path("register/", views.register),
-    path("search/", views.search),
-    path("students/", views.students),
-    path("edit/<int:id>/", views.edit),
-    path("delete/<int:id>/", views.delete),
-    path("python/", views.python_students),
-    path("orm/", views.orm_demo),
-    path("djangoform/", views.django_form, name="django_form"),
+
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+
+    path("register/", views.register, name="register"),
+    path("search/", views.search, name="search"),
+    path("students/", views.students, name="students"),
+
+    path("edit/<int:id>/", views.edit, name="edit"),
+    path("delete/<int:id>/", views.delete, name="delete"),
+
+    path("python/", views.python_students, name="python"),
+    path("orm/", views.orm_demo, name="orm"),
+
+    # IMPORTANT
+    path("djangoform/", views.django_form, name="djangoform"),
+
     path("signup/", views.signup, name="signup"),
     path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
