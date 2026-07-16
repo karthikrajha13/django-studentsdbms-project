b66be49a35fee5b8232181f22edb2fb3d53fe8af
karthikrajha13 <karthikrajha849@gmail.com>
2026-07-16 16:51:09 +0530

    MySQL linked as DataBase

--- project/settings.py
+++ project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_student_db',
+        'USER': 'root',
+        'PASSWORD': 'Karthik@123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
