7b67e1c6793f2bce5e846fe0253c481e0cbcbc9e
karthikrajha13 <karthikrajha849@gmail.com>
2026-07-21 16:05:02 +0530

    Updated

--- app/urls.py
+++ app/urls.py
@@ -1,29 +1,29 @@
 from django.urls import path
 from . import views
 from .views import student_api
+from .views import StudentAPIView
 
 urlpatterns = [
     path('', views.home, name="home"),
 
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
-
     path("register/", views.register, name="register"),
     path("search/", views.search, name="search"),
     path("students/", views.students, name="students"),
-
     path("edit/<int:id>/", views.edit, name="edit"),
     path("delete/<int:id>/", views.delete, name="delete"),
-
     path("python/", views.python_students, name="python"),
     path("orm/", views.orm_demo, name="orm"),
-
-    # IMPORTANT
     path("djangoform/", views.django_form, name="djangoform"),
-
     path("signup/", views.signup, name="signup"),
     path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path('api/students/', views.student_api, name='student_api'),
     path('api/students/<int:id>/', views.student_detail, name='student_detail'),
+    path(
+    'cbv/students/',
+    StudentAPIView.as_view(),
+    name='student_api_cbv'
+)
 ]
--- app/views.py
+++ app/views.py
@@ -13,6 +13,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import StudentSerializer
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
 def home(request):
     return render(request, "app/index.html")
@@ -252,3 +253,16 @@ def student_detail(request, id):
             {"message": "Student deleted successfully"},
             status=status.HTTP_204_NO_CONTENT
         )
+        
+class StudentAPIView(APIView):
+
+    def get(self, request):
+
+        students = Student.objects.all()
+
+        serializer = StudentSerializer(
+            students,
+            many=True
+        )
+
+        return Response(serializer.data)
